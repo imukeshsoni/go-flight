ec3e3e66f9152fe5f929040ade6f25ea6d22d0e8
Mukesh Soni <msony552@gmail.com>
2021-05-21 19:18:03 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/home/index";
+import Navbar from "./components/navbar/index";
+import Login from "./pages/login/index";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/" exact component={Login} />
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
