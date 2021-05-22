66eb5db3c195a1552772b0c2c55de370261a8139
Mukesh Soni <msony552@gmail.com>
2021-05-22 13:31:29 +0530

    packages and urls added

---
+++ src/api-urls.js
@@ -0,0 +1,33 @@
+const createUser = "http://localhost:8084/user/create/";
+const deleteUserById = "http://localhost:8084/user/delete/";
+const getUserById = "http://localhost:8084/user/get/";
+const getAllUsers = "http://localhost:8084/user/getall";
+const updateUser = "http://localhost:8084/user/update/";
+
+const createBooking = "http://localhost:8085/booking/create";
+const getBookingById = "http://localhost:8085/booking/get/";
+const getAllBookings = "http://localhost:8085/booking/getall";
+const updateBookingById = "http://localhost:8085/booking/update/";
+
+const createFlight = "http://localhost:8085/flight/create/";
+const deleteFlightById = "http://localhost:8085/flight/delete/";
+const getFlightById = "http://localhost:8085/flight/get/";
+const getAllFlights = "http://localhost:8085/flight/getall";
+const updateFlight = "http://localhost:8085/flight/update/";
+
+export {
+  createUser,
+  deleteUserById,
+  getUserById,
+  getAllUsers,
+  updateUser,
+  createBooking,
+  getBookingById,
+  getAllBookings,
+  updateBookingById,
+  createFlight,
+  deleteFlightById,
+  getFlightById,
+  getAllFlights,
+  updateFlight,
+};
