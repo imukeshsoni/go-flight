72fa48ab1b4190289f4ad7a665c3dcfa5a874b4f
Mukesh Soni <msony552@gmail.com>
2021-05-24 20:50:46 +0530

    user details component added

--- src/components/user-profile/index.js
+++ src/components/user-profile/index.js
@@ -1,7 +1,37 @@
 import React from "react";
 
 function UserProfile() {
-  return <div>User profile</div>;
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th colSpan="2">User Details</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>User Name</td>
+            <td>{user.name}</td>
+          </tr>
+          <tr>
+            <td>User Email</td>
+            <td>{user.email}</td>
+          </tr>
+          <tr>
+            <td>User Phone</td>
+            <td>{user.phone}</td>
+          </tr>
+          <tr>
+            <td>User Gender</td>
+            <td>{user.gender}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default UserProfile;
