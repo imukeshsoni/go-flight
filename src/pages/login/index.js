46ae0f91f3f12a3d9e048369b098318a7a01b6e4
Mukesh Soni <msony552@gmail.com>
2021-05-25 21:44:36 +0530

    login image fixed

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -32,38 +32,36 @@ function Login() {
   }
 
   return (
-    <div class="container2">
-      <div className="container">
-        <div className="items__container">
-          <form className="login__form" onSubmit={(e) => handleLogin(e)}>
-            <h1>Login here</h1>
-            <h4>Email</h4>
-            <input
-              type="email"
-              value={email}
-              placeholder="Please Enter your email"
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-            <h4>Password</h4>
-            <input
-              type="password"
-              value={password}
-              placeholder="Please enter your password"
-              autoComplete="current-password"
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-            {userWarning && <p className="warning">{userWarning}</p>}
+    <div className="container">
+      <div className="items__container">
+        <form className="login__form" onSubmit={(e) => handleLogin(e)}>
+          <h1>Login here</h1>
+          <h4>Email</h4>
+          <input
+            type="email"
+            value={email}
+            placeholder="Please Enter your email"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <h4>Password</h4>
+          <input
+            type="password"
+            value={password}
+            placeholder="Please enter your password"
+            autoComplete="current-password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          {userWarning && <p className="warning">{userWarning}</p>}
 
-            <button type="submit" className="login__btn">
-              Login
-            </button>
-            <button className="login__btn" onClick={() => setuserSignUp(true)}>
-              Register?
-            </button>
-          </form>
-        </div>
+          <button type="submit" className="login__btn">
+            Login
+          </button>
+          <button className="login__btn" onClick={() => setuserSignUp(true)}>
+            Register?
+          </button>
+        </form>
       </div>
     </div>
   );
